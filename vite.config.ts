219f449647b0a06b96c4d93925a93852936748fd
rchaves77 <romulochaves77@gmail.com>
2026-06-23 18:22:34 -0500

    chore: atualiza transformador jsx para o padrao moderno

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,11 @@
 import path from "path";
-import react from "@vitejs/plugin-react-swc"; // Trocamos para o SWC
+import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [
-    react(), // Limpo e ultra-rápido
+    react({ jsxRuntime: 'automatic' }),
     tailwindcss(),
   ],
   resolve: { 
